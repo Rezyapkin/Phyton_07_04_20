b6c45f99d4ee97a476ce611ca2586dcabaa441e1
Rezyapkin <rezyapkin@yandex.ru>
2020-04-10 17:34:52 +0300

    Correct to PIP-8

--- lesson_1/task5.py
+++ lesson_1/task5.py
@@ -13,7 +13,7 @@ profit = revenue - expenses
 
 if profit > 0:
     print(f'Фирма сработала в прибыль {profit:.2f}')
-    profitability = profit/revenue*100
+    profitability = profit / revenue*100
     print(f'Рентабельность составила {profitability:.2f} %\n')
     count_personal = input('Введите количество сотрудников предприятия')
     count_personal = int(count_personal)
@@ -21,7 +21,7 @@ if profit > 0:
         print(f'Прибыль на одного сотрудника составила: {profit/count_personal:.2f}')
     else:
         print('Видимо сотрудников заменили роботы)')
-elif profit==0:
+elif profit == 0:
     print(f'Фирма сработала в 0')
 else:
     print(f'Фирма сработала в убыток {-profit:.2f}')
