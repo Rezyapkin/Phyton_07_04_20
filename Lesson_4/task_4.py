08d25193d5ba2517c42ffdd26d71a3cc3c071c91
Rezyapkin <rezyapkin@yandex.ru>
2020-04-17 22:53:08 +0300

    Task 4 in lesson 4 complete

---
+++ Lesson_4/task_4.py
@@ -0,0 +1,11 @@
+
+# Задание 4, урок 4
+
+import random
+
+#Сгенерируем список со случайными элементами
+random_list = [random.randint(0,10) for _ in range(20)]
+print(f'Исходный список: {random_list}')
+
+result_list = [value for index, value in enumerate(random_list) if not value in random_list[:index]]
+print(f'Итоговый список: {result_list}')
