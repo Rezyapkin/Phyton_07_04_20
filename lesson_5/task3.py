291f36a667e1b66c9c5567c442e2c3b595196212
Rezyapkin <rezyapkin@yandex.ru>
2020-04-21 23:06:03 +0300

    Complete task 3 lesson 5

---
+++ lesson_5/task3.py
@@ -0,0 +1,24 @@
+'''
+Задание 3. Создать текстовый файл (не программно), построчно записать фамилии сотрудников и величину их окладов.
+Определить, кто из сотрудников имеет оклад менее 20 тыс., вывести фамилии этих сотрудников.
+Выполнить подсчет средней величины дохода сотрудников.
+'''
+
+# В файле фамилии и з/п специально введены с лишними пробелами
+file_name = 'file_task3.txt'
+
+with open(file_name, 'rt', encoding='UTF-8') as file:
+    lines = file.readlines()
+
+    try:
+        values = list(map(lambda value: float(value.replace(' ','')), lines[1::2]))
+
+        print('-' * 30)
+        for staff in lines[::2]:
+            print(staff.strip())
+        print('-' * 30)
+
+        print(f'Средний заработок: {sum(values)/len(values):.2f}')
+
+    except ValueError:
+        print('Ошибочный формат файла')
