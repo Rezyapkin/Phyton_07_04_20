09114b269af255df08fbfb32e1210faa584abd28
Rezyapkin <rezyapkin@yandex.ru>
2020-04-21 20:22:04 +0300

    Task 4 in lesson 4 (не правильно понял условие), думал первое вхождение нужно.

--- Lesson_4/task_4.py
+++ Lesson_4/task_4.py
@@ -7,5 +7,5 @@ import random
 random_list = [random.randint(0, 10) for _ in range(20)]
 print(f'Исходный список: {random_list}')
 
-result_list = [value for index, value in enumerate(random_list) if not value in random_list[:index]]
+result_list = [value for value in random_list if random_list.count(value) == 1]
 print(f'Итоговый список: {result_list}')
